27bdff851d60f8ae358222fc618acb524c09778f
Priyansh Tiwari <32665685+priyansh14@users.noreply.github.com>
2019-04-14 02:06:46 +0530

    Function for main menu and introduction

---
+++ screenshow.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<Windows.h>
+int main()
+{
+	
+	printf("\n\n\n\n");
+	system("color 0B");
+	printf("\t\t\t  CSE316 :: Operating Systems Project ");
+	
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	Sleep(1000);
+	printf("\t\t\t  Name :: Priyansh Tiwari ");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	
+	Sleep(1200);
+	system("color 02");
+	printf("\t\t\t  Reg. Number :: 11711226");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	Sleep(1200);
+	printf("\t\t\t  Roll Number :: 13");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	Sleep(1200);
+	printf("\t\t\t  Section :: K17KV");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	Sleep(1200);
+	printf("\t\t\t  Submitted to :: Sumit Mittu Sir on 14th April 2019 ");
+}
